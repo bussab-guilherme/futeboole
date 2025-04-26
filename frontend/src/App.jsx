a922c835f1511c49862efcb09ba909aa6a062cea
Pedro Bussab <pedromonas22@gmail.com>
2025-04-26 20:29:43 -0300

    sistema de sign-up/login/autenticação implementado

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,30 +2,83 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [nusp, setNusp] = useState("");
-  const [name, setName] = useState("");
+  const [username, setName] = useState("");
   const [password, setPassword] = useState("");
   const [message, setMessage] = useState("");
 
   const sendUserData = async () => {
     try {
-      const response = await fetch("/api/users", {
+      const response = await fetch("/api/users/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
         },
-        body: JSON.stringify({ nusp, name, password, score: 0 }),
+        body: JSON.stringify({username, password, playerScore: 0, teamScore: 0, numVotes: 0, team: []}),
       });
       if (response.ok) {
         const data = await response.text();
         setMessage(data);
+      
       } else {
-        setMessage(`Error: ${response.status}`);
+          const errorMessage = await response.text();
+          setMessage(`Error: ${response.status}, ${errorMessage}`);
       }
     } catch (error) {
       setMessage("Error sending data");
     }
   };
+
+  const sendLogin = async () => {
+    try {
+      const response = await fetch("/api/users/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({username, password, playerScore: 0, teamScore: 0, numVotes: 0, team: []}),
+      });
+      if (response.ok) {
+        const data = await response.text();
+        setMessage(data);
+      
+      } else {
+          const errorMessage = await response.text();
+          setMessage(`Error: ${response.status}, ${errorMessage}`);
+      }
+    } catch (error) {
+      setMessage("Error sending data");
+    }
+  };
+
+  const getProfile = async () => {
+    const response = await fetch("/api/users/profile", {
+      method: "GET",
+    });
+
+    if (response.ok) {
+      const data = await response.text();
+      setMessage(data);
+    
+    } else {
+        const errorMessage = await response.text();
+        setMessage(`Error: ${response.status}, ${errorMessage}`);
+    }
+  }
+  
+  const sendLogout = async () => {
+    const response = await fetch("/api/users/logout", {
+      method: "POST",
+    });
+
+    if (response.ok) {
+      const data = await response.text();
+      setMessage(data);
+    
+    } else {
+        const errorMessage = await response.text();
+        setMessage(`Error: ${response.status}, ${errorMessage}`);
+    }
+  }
 
   return (
     <div>
@@ -33,16 +86,8 @@ function App() {
       <div>
         <input
           type="text"
-          placeholder="NUSP"
-          value={nusp}
-          onChange={(e) => setNusp(e.target.value)}
-        />
-      </div>
-      <div>
-        <input
-          type="text"
           placeholder="Name"
-          value={name}
+          value={username}
           onChange={(e) => setName(e.target.value)}
         />
       </div>
@@ -55,6 +100,9 @@ function App() {
         />
       </div>
       <button onClick={sendUserData}>Send Data</button>
+      <button onClick={sendLogin}>Login</button>
+      <button onClick={sendLogout}>Logout</button>
+      <button onClick={getProfile}>Profile</button>
       {message && <p>{message}</p>}
     </div>
   );
