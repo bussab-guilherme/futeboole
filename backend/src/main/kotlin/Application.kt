af7fb3aeb4a1959da70a049eb84ea5ab43166343
Pedro Bussab <pedromonas22@gmail.com>
2025-06-25 18:26:17 -0300

    merge com a bracnh do market-back

--- backend/src/main/kotlin/Application.kt
+++ backend/src/main/kotlin/Application.kt
@@ -15,6 +15,7 @@ fun Application.module() {
     configureSecurity()
     configureSessionAuthentication()
     configureSerialization()
-    configureDatabases()
+    configureDatabases(true)
     configureRouting()
+
 }
